6582a960a98f74a004b852bafa6d0c9241a5df82
huangshenglong <925765504@qq.com>
2020-05-12 22:29:31 +0800

    求职者与招聘者接口优化

---
+++ src/routes/JobSeeker/Detail/Detail.js
@@ -0,0 +1,174 @@
+import React, {Component, Fragment} from 'react';
+import {connect} from 'dva';
+import {Icon, Form, Input, Button, message, Table, Alert, Badge, Card, Divider, Popconfirm, Modal} from 'antd';
+import Style from "./style.less";
+import * as Data from '../../data/data';
+import PaginationTable from '../../components/PaginationTable/PaginationTable';
+import * as RecruitApi from '../../services/RecruitApi';
+import {isEmpty} from "../../utils/utils";
+import EditView from "../JobSeeker/EditView";
+
+
+class JobSeekerListView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      loading: false,
+      currIndex: 1,
+      isShowDialog: false,
+      info: {}
+    };
+  }
+
+  componentWillMount() {
+    this.refreshList();
+  }
+
+  refreshList() {
+    let info = {
+      pageIndex: this.state.currIndex,
+      pageSize: Data.PAGINATION_INFO.pageSize
+    };
+
+    RecruitApi.JobSeekerViewList(info, (resp)=> {
+      this.setState({
+        data: resp.data
+      });
+    }, (error)=> {
+      message.error('反馈数据获取失败: ' + JSON.stringify(error))
+    });
+  }
+
+  render() {
+    let {data, loading, isShowDialog, info} = this.state;
+
+    const columns = [
+      {
+        title: '岗位名称',
+        align: 'center',
+        dataIndex: 'JobSeekerName'
+      },
+      {
+        title: '公司名称',
+        align: 'center',
+        dataIndex: 'companyName'
+      },
+      {
+        title: '招聘者',
+        align: 'center',
+        dataIndex: 'realName'
+      },
+      {
+        title: '职位内容',
+        align: 'center',
+        dataIndex: 'JobSeekerContent',
+      },
+      {
+        title: '职位类型',
+        align: 'center',
+        dataIndex: 'positionTypeName',
+      },
+      {
+        title: '图片',
+        align: 'center',
+        dataIndex: 'image',
+        render: (val, record, index) => (<img className={Style.listItemIcon} src={val} alt="暂无图片"/>)
+      },
+      {
+        title: '操作',
+        align: 'center',
+        dataIndex: 'id',
+        render: (val, record) => (<div>
+            <Button className={Style.mainOperateBtn}  onClick={() => this.onEdit(record)} type="normal" shape="circle" icon="edit"/>
+
+            <Popconfirm title="是否要删除该关于？"
+                        onConfirm={() => {
+                          this.onDelClick(record.id)
+                        }}
+                        okText="确定" cancelText="取消">
+              <Button type="normal" shape="circle" icon="delete"/>
+            </Popconfirm>
+
+          </div>
+        ),
+      },
+    ];
+
+    return (
+      <div>
+        {isShowDialog &&
+        <Modal
+          style={{marginBottom: '30rem'}}
+          destroyOnClose="true"
+          width={820}
+          title={isEmpty(info) ? '新增反馈' : '编辑反馈'}
+          onCancel={() => this.onDialogDismiss()}
+          visible={true}
+          footer={null}
+        >
+          <EditView
+            info={info}
+            onDialogDismiss={()=> this.onDialogDismiss(true)}
+          />
+        </Modal>
+        }
+
+        <div className={Style.btnLayout}>
+          <Button className={Style.mainOperateBtn} type="primary" onClick={() => {
+            this.refreshList()
+          }}>刷新</Button>
+          <Button className={Style.mainOperateBtn} type="primary" onClick={() => {
+            this.onEdit(null)
+          }}>添加</Button>
+        </div>
+
+        <PaginationTable
+          dataSource={data}
+          loading={loading}
+          columns={columns}
+          onPageChange={(page, pageSize)=> {
+            this.onPageChange(page, pageSize)
+          }}
+        />
+
+      </div>);
+  }
+
+  onPageChange(page, pageSize) {
+    this.setState({
+      currIndex: page.current,
+    }, ()=> {
+      this.refreshList();
+    });
+  }
+
+  onEdit(info) {
+    this.setState({
+      info: info,
+      isShowDialog: true,
+    });
+  }
+
+  onDelClick(id) {
+    RecruitApi.deleteJobSeeker(id, (resp)=> {
+      message.success('删除反馈成功');
+      this.refreshList();
+    }, (error)=> {
+
+    });
+  }
+  onDialogDismiss(showRefresh= false) {
+    this.setState({
+      isShowDialog: false
+    }, () => {
+      if(showRefresh) {
+        this.refreshList();
+      }
+    })
+  }
+
+}
+
+export default JobSeekerListView;
