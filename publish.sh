f8634ec9d52afae07c5e64678923ef36d7da3dd4
huangbin <575395982@qq.com>
2020-06-06 22:56:10 +0800

    MOD 修改发布脚本 publish.sh 中服务器路径

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#npm run build
-#scpct ./dist/* //opt/crafttime/recruit/cms
+npm run build
+#scpct ./dist/* //opt/crafttime/recruit/RecruitCMS
 
-sudo scp -i //Users/Bingo/Development/OneZeroBeat/Product/CraftTime/Recruit/Dev/centus.pem -r ./dist/* centos@34.229.235.238://opt/crafttime/recruit/cms
+sudo scp -i //Users/Bingo/Development/OneZeroBeat/Product/CraftTime/Recruit/Dev/centus.pem -r ./dist/* centos@34.229.235.238://opt/crafttime/recruit/RecruitCMS
