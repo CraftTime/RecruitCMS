5a871c3aec2103d6dbbb6586f806467129d382c8
SleepyFox <1032957704@qq.com>
2021-02-24 10:39:18 +0800

    亚马逊证书位置修改

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 npm run build
 #scpct ./dist/* //opt/crafttime/recruit/RecruitCMS
 
-sudo scp -i //Users/Bingo/Development/OneZeroBeat/Product/CraftTime/Recruit/Dev/centus.pem -r ./dist/* centos@34.229.235.238://opt/crafttime/recruit/RecruitCMS
+sudo scp -i //Users/sleepyfox/Downloads/production/centus.pem -r ./dist/* centos@34.229.235.238://opt/crafttime/recruit/RecruitCMS
